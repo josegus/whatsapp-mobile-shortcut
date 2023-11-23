02e34c7f63581c622b1232c9101f010b80cc8a77
Gustavo Vasquez <--get>
2023-11-23 14:09:25 -0400

    Add service worker files

---
+++ sw-main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const CACHE_NAME = 'cache-v1';
+
+// The files we want to cache
+const resourceList = [
+    '/',
+    'index.html',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => {
+        return cache.addAll(resourceList);
+    }));
+});
+
+function addToCache(cacheName, resourceList) {
+    caches.open(cacheName).then(cache => {
+        return cache.addAll(resourceList);
+    });
+}
+
+self.addEventListener('fetch', event => {
+    event.respondWith(caches.match(event.request).then(response => {
+        return response || fetch(event.request);
+    }));
+});
